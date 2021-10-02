2e399f61d8b840fc8b247883a31d033d3a24d94a
Igor Kislitsyn <k.yngvarr@gmail.com>
2021-10-02 22:12:38 +0700

    penguin moves to a button

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
+/**** CLASSES ****/
+
 class Penguin {
     constructor(x, y, container) {
         this.x = x;
         this.y = y;
         this.el = this.initElement(container);
+        // point
+        this.target = null;
 
         this.updateView();
     }
@@ -32,11 +36,31 @@ class Penguin {
     }
 }
 
+/**** GLOBAL STATE ****/
+
 const state = {
     prevFrame: null,
 
     penguins: []
 };
+
+/**** HELPERS ****/
+
+function getCenter(el) {
+    const rect = el.getBoundingClientRect();
+    return { x: rect.x + rect.width / 2, y: rect.y + rect.height / 2 };
+}
+
+function distance(from, to) {
+    return Math.sqrt((to.x - from.x) ** 2 + (to.y - from.y) ** 2);
+}
+
+function direction(from, to) {
+    const dist = distance(from, to);
+    return { x: (to.x - from.x) / dist, y: (to.y - from.y) / dist };
+}
+
+/**** GAMEPLAY ****/
 
 function step(t) {
     requestAnimationFrame(step);
@@ -45,20 +69,22 @@ function step(t) {
         state.prevFrame = t;
     }
     const dt = t - state.prevFrame;
+    state.prevFrame = t;
 
     if (dt > 1e3) return;
 
     for (let p of state.penguins) {
-        p.move(1, 0);
+        if (p.target === null) continue;
+        const dir = direction(p, p.target);
+        p.move(dir.x, dir.y);
     }
-
-    state.prevFrame = t;
 }
 
 function init() {
     const body = document.querySelector('body');
 
     state.penguins = [new Penguin(10, 10, body)];
+    state.penguins[0].target = getCenter(document.querySelector('#no-clickin'));
 
     requestAnimationFrame(step);
 }
