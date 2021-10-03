d18ece04be376ebd9573a4bfff595c57307609df
Вероника Симагина <syudzius@gmail.com>
2021-10-03 20:38:04 +0700

    fix idle

--- js/main.js
+++ js/main.js
@@ -341,7 +341,7 @@ function init() {
     const t = getCenter(game.target);
 
     const frames = {
-        idle: [[0,0], [0,1]],
+        idle: [[0,0], [1,0]],
 
         up: [[0,1], [1,1],[0,1],[2,1]],
         down: [[1,0], [2,0],[1,0],[3,0]],
